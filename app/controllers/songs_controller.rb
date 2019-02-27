5333e2ce59d54993d5976868d2381e4e7006c026
YDick <ydick1998@gmail.com>
2019-02-27 14:51:10 -0500

    working on new/create, made root = index, fixed typo on index url => picture

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,4 +3,21 @@ class SongsController < ApplicationController
     def index
         @songs = Song.all
     end
+
+    def new
+        @song = Song.new
+    end
+
+    def create
+        @song = Song.create(song_params)
+
+        if @song.save
+            redirect_to "/songs"
+        else
+            render :new
+        end
+
+    end
+        
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  root "songs#index"
+
   get 'songs' => 'songs#index'
+
+  post 'songs' => 'songs#create'
+
+  get 'songs/new' => 'songs#new'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
