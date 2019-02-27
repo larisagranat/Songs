375e065d3c320d0867a65e86cc51c9bf8ef028f2
larisagranat <larisa.granat@gmail.com>
2019-02-26 22:34:46 -0500

    created show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'songs' => 'songs#index'
-  
+  get 'songs/:id' => 'songs#show', as:'song'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
