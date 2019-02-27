27066d19f96486cd0115891079a46f0b8b48f7c6
YudiR <yudrose13@gmail.com>
2019-02-27 12:04:52 -0500

    edit & update complete. need to test them with show

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,24 @@
 class SongsController < ApplicationController
+
+
+    def edit 
+    @song = Song.find(params[:id]) 
+    end
+
+    def update
+        @song = Song.find(params[:id])
+
+        if @song.update_attributes(songs_params)
+            redirect_to "/songs/#{@song.id}"
+        else
+            render :edit
+        end
+    end
+
+
+    private
+        def songs_params
+            params.require(:song).permit(:title, :singer,  :genre, :picture)
+        end
+
 end
