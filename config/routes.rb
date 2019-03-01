6bfc4d153b933fbe31e1dcd17b2fc2bdfeb2dbfb
YudiR <yudrose13@gmail.com>
2019-02-28 20:13:19 -0500

    fixed errors in the code. Edit and update are up and running.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'songs/:id/edit' => "songs#edit", as: "edit_song"
-  patch 'songs/:id' => "songs#update"
+  patch 'songs/:id' => "songs#update" , as: "song"
 end
